625b94147c1f1d5eeb7ae8c2a735aede25223097
ContaDoge <mgerst23@student.aau.dk>
2023-11-21 11:15:05 +0100

    MenuManager + ændringer til Game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,4 +15,15 @@ public class GameManager : MonoBehaviour
     {
         
     }
+    bool GameHasEnded = false;
+    public GameObject GameOverBackground;
+    public void RestartGame()
+    {
+        if (GameHasEnded == true)
+        {
+            GameHasEnded = false;
+            GameOverBackground.SetActive(false);
+            Debug.Log("New Game");
+        }
+    }
 }
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Next Scene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Game Screen");
+        FindObjectOfType<GameManager>().RestartGame();
+        Debug.Log("Reloading New Game");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Debug.Log("Loading Menu");
+    }
+}
