e2d73887bc62419e7c7737d2c5e2e4d2d8da7605
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-11-21 13:49:49 +0100

    Card Stack & methods

---
+++ Assets/Scripts/Cards/CardPower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPower : MonoBehaviour
+{
+    public PowerType power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
