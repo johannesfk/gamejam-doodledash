30b436608909757f6abbc1fba9f7cd9517efb61b
BlonkoTron <pooplypoopinton@gmail.com>
2023-11-21 16:55:47 +0100

    movement still needs work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    //Credit:
+    //https://www.youtube.com/watch?v=KbtcEVCM7bw
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    [SerializeField] private float speed = 0;
-    [SerializeField] private float minSpeed = 0.5f;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float minSpeed = 1;
     [SerializeField] private float maxSpeed = 5;
-    [SerializeField] private float acceleration = 0.5f;
-    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float acceleration = 0.8f;
+    [SerializeField] private float jumpSpeed = 8;
 
     private bool isGrounded;
     private Vector2 movement;
+    
+
 
     private void Start()
     {
@@ -25,26 +29,25 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update()
-    {   
-        if (movement.x <= 0)
+    {
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+
+        if (movement.x == 0)
         {   
             if (speed >= minSpeed)
             {
                 speed -= acceleration * Time.deltaTime;
             }
         }
-        if (movement.x >= 0)
+        else
         {
             if (speed <= maxSpeed) 
             {
-                
+                speed += acceleration * Time.deltaTime;
             }
         }
-
-        if (rb.velocity.y <= 0)
-
-
-        rb.velocity = new Vector2 (movement.x * speed, rb.velocity.y);
+        
+        
     }
 
 
