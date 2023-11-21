e964cd43db7caedc211f3df2dc52fa432e64b53f
BlonkoTron <pooplypoopinton@gmail.com>
2023-11-21 11:13:53 +0100

    her victor din ged
    hahaha jk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    [SerializeField] private int speed;
+
+
+
+
+}
