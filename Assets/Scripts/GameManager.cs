98a518fef1643825459c2a96e3e4499db9817b73
Akaliias <akaliasthegamer@gmail.com>
2023-11-21 11:37:47 +0100

    Timer and Pause screen done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float timer;
+
+    public GameObject pauseScreen;
 
 
 
@@ -15,7 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseScreen.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -32,4 +36,28 @@ public class GameManager : MonoBehaviour
         int seconds = Mathf.FloorToInt(timer % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Debug.Log("Game is paused");
+        pauseScreen.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Game is resumed");
+        pauseScreen.SetActive(false);
+    }
+
+    public void OnPauseMenu(InputValue button)
+    {
+        Pause();
+        Debug.Log("hotkey: pause");
+       
+
+    }
+
+
 }
