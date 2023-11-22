f7750abd92d0c6e18c00643135d7d382b91be57d
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-11-22 10:11:46 +0100

    Changed to CardCollectable + enabled sprite change

--- Assets/Scripts/Cards/CardStyles.cs
+++ Assets/Scripts/Cards/CardStyles.cs
@@ -10,8 +10,19 @@ public class CardStyles : MonoBehaviour
     public Color wallJumpColor;
     public Color bounceColor;
     public Color teleportColor;
+    [SerializeField]
+    private Sprite dashSprite;
+    [SerializeField]
+    private Sprite doubleJumpSprite;
+    [SerializeField]
+    private Sprite wallJumpSprite;
+    [SerializeField]
+    private Sprite bounceSprite;
+    [SerializeField]
+    private Sprite teleportSprite;
 
     public Dictionary<PowerType, Color> cardColors;
+    public Dictionary<PowerType, Sprite> cardSprites;
 
     private void Awake()
     {
@@ -22,6 +33,14 @@ public class CardStyles : MonoBehaviour
             [PowerType.WallJump] = wallJumpColor,
             [PowerType.Bounce] = bounceColor,
             [PowerType.Teleport] = teleportColor
+        };
+        cardSprites = new Dictionary<PowerType, Sprite>
+        {
+            [PowerType.Dash] = dashSprite,
+            [PowerType.DoubleJump] = doubleJumpSprite,
+            [PowerType.WallJump] = wallJumpSprite,
+            [PowerType.Bounce] = bounceSprite,
+            [PowerType.Teleport] = teleportSprite
         };
     }
 }
--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,39 +4,8 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public PowerType power;
-    public CardStack cardStack;
-
-    private CardStyles cardStyles;
-
-    private void Awake()
-    {
-        cardStyles = cardStack.GetComponentInParent<CardStyles>();
-
-        Color cardColor = cardStyles.cardColors[power];
-
-        GetComponent<SpriteRenderer>().color = cardColor;
-
-
-        /* switch (power)
-        {
-            case PowerType.Dash:
-                SetSpriteColor();
-                break;
-            default:
-                break;
-        } */
-    }
-
     private void OnDestroy()
     {
-        Debug.Log("Tilføjer " + power + " til kortstakken");
-        cardStack.Add(power);
+        Debug.Log("Collectable destroyed");
     }
-
-    /* private void SetSpriteColor(Color cardStyle)
-    {
-
-        GetComponent<SpriteRenderer>().color = cardStyle;
-    } */
 }
