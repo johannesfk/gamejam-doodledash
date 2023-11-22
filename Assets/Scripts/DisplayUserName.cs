557c0a6d3beea0a4e575ea865178db6bc8e6f1c5
ContaDoge <mgerst23@student.aau.dk>
2023-11-22 13:37:06 +0100

    Menu Almost Done + Username

---
+++ Assets/Scripts/DisplayUserName.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayUserName : MonoBehaviour
+{
+    public Text obj_text;
+    public InputField display;
+
+    void Start()
+    {
+        obj_text.text = PlayerPrefs.GetString("User_name");
+
+    }
+    public void DisplayText()
+    {
+        obj_text.text = display.text;
+        PlayerPrefs.SetString("User_name", obj_text.text);
+        PlayerPrefs.Save();
+    }
+}
