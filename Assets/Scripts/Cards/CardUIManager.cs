0ec18edf77717ff1a9dc035c9e06aa97293ca7fa
ContaDoge <mgerst23@student.aau.dk>
2023-11-23 14:26:47 +0100

    Card script

--- Assets/Scripts/Cards/CardUIManager.cs
+++ Assets/Scripts/Cards/CardUIManager.cs
@@ -3,8 +3,8 @@ using UnityEngine.UI;
 
 public class CardUIManager : MonoBehaviour
 {
-    public Text currentCardText;
-    public Text nextCardText;
+    public Image currentCardImage;
+    public Image nextCardImage;
 
     public CardStack cardStack;
 
@@ -15,28 +15,9 @@ public class CardUIManager : MonoBehaviour
 
     private void UpdateUI()
     {
-        if (cardStack.cards.Count > 0)
-        {
-            // Display the current card
-            currentCardText.text = "Current Card: " + cardStack.cards[0].power.ToString();
-        }
-        else
-        {
-            currentCardText.text = "No cards left";
-        }
 
-        if (cardStack.cards.Count > 1)
-        {
-            // Display the next card
-            nextCardText.text = "Next Card: " + cardStack.cards[1].power.ToString();
-        }
-        else
-        {
-            nextCardText.text = "No next card";
-        }
     }
 
-    // Call this method when you add or use a card in the CardStack
     public void OnCardStackChanged()
     {
         UpdateUI();
