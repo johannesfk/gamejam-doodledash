137f7d6ce01270e6e64ac500ed7d5314bc9481c5
ContaDoge <mgerst23@student.aau.dk>
2023-11-23 13:17:29 +0100

    Moving Platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float Speed;
+    public int startingpoint;
+    public Transform[] points;
+
+
+    private int i;
+
+    void Start()
+    {
+       transform.position = points[startingpoint].position;     
+    }
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.2f) 
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i= 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, Speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (transform.position.y < collision.transform.position.y-0.5f) 
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
+
